70bd44f4ef34813ddc1604ffd6a7f5db329621cf
Joao Victor da Silva Coutinho <joao.coutinho@pampa.compasso>
2021-09-20 18:50:44 -0300

    Recorte das opções do Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,6 +4,7 @@ import ProfilePicture from "../ProfilePicture";
 import Input from "../Input";
 import {
   HeaderArea,
+  HeaderUserArea,
   InfoArea,
   TopInfoArea,
   TopInfoAreaButtons,
@@ -13,73 +14,129 @@ import {
   UserMessageText,
   BottomInfoAreaChat,
   BottomButtonsArea,
+  HeaderOptions,
+  HeaderItem,
+  HeaderItemIcon,
+  HeaderItemText,
 } from "./styles";
-import { Ionicons } from "@expo/vector-icons";
+import {
+  FontAwesome,
+  Ionicons,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 const Header = ({ chatHeader, goBack }) => {
   return (
     <HeaderArea>
-      <ProfilePicture
-        colors={["#fff", "#C3EFCB", "#39CA54"]}
-        source={require("../../assets/jacquin.png")}
-      />
-      <InfoArea>
-        <TopInfoArea>
-          <View>
-            <UserNameText>Tyrone Fuller</UserNameText>
-            <UserStatusText>(Online)</UserStatusText>
-          </View>
-          <TopInfoAreaButtons>
+      <HeaderUserArea>
+        <ProfilePicture
+          colors={["#fff", "#C3EFCB", "#39CA54"]}
+          source={require("../../assets/jacquin.png")}
+        />
+        <InfoArea>
+          <TopInfoArea>
+            <View>
+              <UserNameText>Tyrone Fuller</UserNameText>
+              <UserStatusText>(Online)</UserStatusText>
+            </View>
+            <TopInfoAreaButtons>
+              {chatHeader ? (
+                <TouchableOpacity
+                  style={{ marginRight: 10 }}
+                  onPress={() => {
+                    goBack();
+                  }}
+                >
+                  <Ionicons name="arrow-back" size={36} color="#4E609B" />
+                </TouchableOpacity>
+              ) : (
+                <>
+                  <TouchableOpacity style={{ marginRight: 10 }}>
+                    <Ionicons name="search-sharp" size={24} color="#4E609B" />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Ionicons name="settings-sharp" size={24} color="#4E609B" />
+                  </TouchableOpacity>
+                </>
+              )}
+            </TopInfoAreaButtons>
+          </TopInfoArea>
+          <BottomInfoArea>
             {chatHeader ? (
-              <TouchableOpacity
-                style={{ marginRight: 10 }}
-                onPress={() => {
-                  goBack();
-                }}
-              >
-                <Ionicons name="arrow-back" size={36} color="#4E609B" />
-              </TouchableOpacity>
+              <BottomInfoAreaChat>
+                <UserMessageText>Carpe Diem!</UserMessageText>
+                <BottomButtonsArea>
+                  <TouchableOpacity style={{ marginRight: 10 }}>
+                    <Ionicons
+                      name="videocam-outline"
+                      size={28}
+                      color="#4e609b"
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Ionicons name="call-outline" size={24} color="#4e609b" />
+                  </TouchableOpacity>
+                </BottomButtonsArea>
+              </BottomInfoAreaChat>
             ) : (
-              <>
-                <TouchableOpacity style={{ marginRight: 10 }}>
-                  <Ionicons name="search-sharp" size={24} color="#4E609B" />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Ionicons name="settings-sharp" size={24} color="#4E609B" />
-                </TouchableOpacity>
-              </>
+              <Input
+                textSize="11px"
+                textAlign="center"
+                placeholder="Share a message or link with your contacts"
+                placeholderTextColor="#929bb3"
+                color="#4e609b"
+                backgroundColor="#ffffff"
+                borderSize="1px"
+                borderColor="#929bb3"
+                borderRadius="50px"
+                padding="0 5px"
+              />
             )}
-          </TopInfoAreaButtons>
-        </TopInfoArea>
-        <BottomInfoArea>
-          {chatHeader ? (
-            <BottomInfoAreaChat>
-              <UserMessageText>Carpe Diem!</UserMessageText>
-              <BottomButtonsArea>
-                <TouchableOpacity style={{ marginRight: 10 }}>
-                  <Ionicons name="videocam-outline" size={28} color="#4e609b" />
-                </TouchableOpacity>
-                <TouchableOpacity>
-                  <Ionicons name="call-outline" size={24} color="#4e609b" />
-                </TouchableOpacity>
-              </BottomButtonsArea>
-            </BottomInfoAreaChat>
-          ) : (
-            <Input
-              textSize="11px"
-              textAlign="center"
-              placeholder="Share a message or link with your contacts"
-              placeholderTextColor="#929bb3"
+          </BottomInfoArea>
+        </InfoArea>
+      </HeaderUserArea>
+      <HeaderOptions>
+        <TouchableOpacity>
+          <HeaderItem>
+            <HeaderItemIcon>
+              <FontAwesome name="user" size={24} color="#4e609b" />
+            </HeaderItemIcon>
+            <HeaderItemText>Profile Picture</HeaderItemText>
+          </HeaderItem>
+        </TouchableOpacity>
+        <HeaderItem>
+          <HeaderItemIcon>
+            <FontAwesome name="dot-circle-o" size={24} color="#4e609b" />
+          </HeaderItemIcon>
+          <HeaderItemText>Status</HeaderItemText>
+        </HeaderItem>
+        <HeaderItem>
+          <HeaderItemIcon>
+            <MaterialCommunityIcons
+              name="camera-outline"
+              size={24}
               color="#4e609b"
-              backgroundColor="#ffffff"
-              borderSize="1px"
-              borderColor="#929bb3"
-              borderRadius="50px"
-              padding="0 5px"
             />
-          )}
-        </BottomInfoArea>
-      </InfoArea>
+          </HeaderItemIcon>
+          <HeaderItemText>Story</HeaderItemText>
+        </HeaderItem>
+        <HeaderItem>
+          <HeaderItemIcon>
+            <MaterialCommunityIcons
+              name="account-group"
+              size={24}
+              color="#4e609b"
+            />
+          </HeaderItemIcon>
+          <HeaderItemText>Create Group</HeaderItemText>
+        </HeaderItem>
+        <HeaderItem>
+          <HeaderItemIcon>
+            <FontAwesome name="sign-out" size={24} color="#4e609b" />
+          </HeaderItemIcon>
+          <HeaderItemText>Sign Out</HeaderItemText>
+        </HeaderItem>
+      </HeaderOptions>
     </HeaderArea>
   );
 };
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,13 @@
 import styled from "styled-components/native";
 
 export const HeaderArea = styled.View`
+  /* padding: 25px 25px 12.5px;
+  background-color: #d8deef; */
+  position: relative;
+  z-index: 1;
+`;
+
+export const HeaderUserArea = styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
@@ -57,5 +64,34 @@ export const BottomButtonsArea = styled.View`
 
 export const UserMessageText = styled.Text`
   font-size: 12px;
+  color: #4e609b;
+`;
+
+export const HeaderOptions = styled.View`
+  position: absolute;
+  top: 100%;
+  width: 100%;
+  padding: 12.5px 25px 25px;
+  border-radius: 20px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+  background-color: #d8deef;
+`;
+
+export const HeaderItem = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin: 5px 15px;
+`;
+
+export const HeaderItemIcon = styled.View`
+  flex: 1;
+  align-items: center;
+  width: 10%;
+`;
+
+export const HeaderItemText = styled.Text`
+  width: 85%;
+  font-size: 16px;
   color: #4e609b;
 `;
